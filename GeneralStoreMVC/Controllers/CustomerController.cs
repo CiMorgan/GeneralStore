49e72521e0f8c71884cdeec760e65676f84e2b57
CiMorgan <camorgan70@gmail.com>
2021-03-27 13:50:44 -0400

    Customer Create

--- GeneralStoreMVC/Controllers/CustomerController.cs
+++ GeneralStoreMVC/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeneralStoreMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,35 @@ namespace GeneralStoreMVC.Controllers
 {
     public class CustomerController : Controller
     {
+        private ApplicationDbContext _db = new ApplicationDbContext();
         // GET: Customer
         public ActionResult Index()
         {
+            List<Customer> customerList = _db.Customers.ToList();
+            List<Customer> orderedcustList = customerList.OrderBy(cust => cust.LastName).ToList();
+            return View(orderedcustList);
+        }
+
+        // Get: Customer
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        //POST: Customer
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
         }
     }
 }
